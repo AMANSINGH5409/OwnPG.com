ab7a2c873592e337cbea27bbcd4b7ddc554b94fb
Aman Rajan Singh <madhusingh74055@gmail.com>
2023-04-19 17:40:49 +0530

    Server Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { RouterProvider, createBrowserRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 import Home from './components/Home';
 import Auth from './components/Auth';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />
-    }
-]);
 
 const App = () => {
     return (
