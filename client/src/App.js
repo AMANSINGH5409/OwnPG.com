692e24d25926500e463b8d90bfaca604f819ae94
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-04-22 12:27:23 +0530

    Changes in Navbar and Home

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,18 @@ import Auth from './components/Auth';
 
 const App = () => {
     return (
-        <GoogleOAuthProvider clientId='789920631154-pk8u145cc7a8ss3hn9qkakctaq4es404.apps.googleusercontent.com'> 
+        <GoogleOAuthProvider clientId='789920631154-pk8u145cc7a8ss3hn9qkakctaq4es404.apps.googleusercontent.com'>
             <BrowserRouter>
-                <div>
-                    <Navbar />
-                    <Routes>
-                        <Route path='/' exact Component={Home} />
-                        <Route path='/auth' exact Component={Auth} />
-                    </Routes>
+                <div className='app'>
+                    <div className=''>
+                        <Navbar />
+                    </div>
+                    <div className="pt-20">
+                        <Routes>
+                            <Route path='/' exact Component={Home} />
+                            <Route path='/auth' exact Component={Auth} />
+                        </Routes>
+                    </div>
                 </div>
             </BrowserRouter>
         </GoogleOAuthProvider>
